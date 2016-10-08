a6eda37353fac9a6b9708dc4a03f5855beacaaa5
simonscoffins <simonscoffins@gmail.com>
2016-10-08 09:54:44 -0400

    created android project structure

---
+++ IL.Core/Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace IL.Core {
+    public static class Data {
+
+        static Data() {
+            Description = "Intersectional Life";
+        }
+
+        public static string Description { get; set; }
+    }
+}
